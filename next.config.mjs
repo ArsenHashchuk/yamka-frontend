812f2c5e82ff16be30fbd909985f8650f62beaf0
SegfaultDaddy <150715168+SegfaultDaddy@users.noreply.github.com>
2025-11-22 15:15:59 +0200

    Simplify next.config.mjs configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,7 @@
 import withPWAInit from "@ducanh2912/next-pwa";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",        // <-- REQUIRED for GitHub Pages
-  images: {
-    unoptimized: true,     // <-- REQUIRED for export
-  },
-  basePath: "/<your-repo-name>", // OPTIONAL but needed if site is not root
-  assetPrefix: "/<your-repo-name>/",
-};
+const nextConfig = {};
 
 const withPWA = withPWAInit({
   dest: "public",
